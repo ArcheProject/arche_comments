fcf5909a9c44c0b5a45e7c29d82286169b27466b
Robin Harms Oredsson <robin@betahaus.net>
2016-11-29 10:53:31 +0100

    Initial commit

---
+++ arche_comments/fanstatic_lib.py
@@ -0,0 +1,22 @@
+from arche.fanstatic_lib import common_js
+from arche.interfaces import IBaseView
+from arche.interfaces import IViewInitializedEvent
+from fanstatic import Library
+from fanstatic import Resource
+from js.bootstrap import bootstrap_js
+from js.bootstrap import bootstrap_css
+
+
+library = Library('arche_comments', 'static')
+
+arche_comments_css = Resource(library, 'css/main.css', depends = (bootstrap_css,))
+arche_comments_scripts = Resource(library, 'js/scripts.js', depends=(bootstrap_js, common_js))
+
+
+def need_subscriber(view, event):
+    arche_comments_css.need()
+    arche_comments_scripts.need()
+
+
+def includeme(config):
+    config.add_subscriber(need_subscriber, [IBaseView, IViewInitializedEvent])
